a93d1463c3cebdada5ea6093120a239e8f1ab207
Jaimon <jaimon.jacob2009@gmail.com>
2020-01-30 08:13:58 -0500

    new

---
+++ js_simplechallenge/script.js
@@ -0,0 +1,31 @@
+//Coding challenge 4
+/*
+var markInfo = {
+	fullName: "Mark Doe",
+	mass: "40",
+	height: 6,
+	calcBmi: function(){
+		this.bmi = this.mass /(this.height * this.height);
+		return this.bmi;
+	} 
+};
+
+var johnInfo = {
+	fullName: "John Doe",
+	mass: "30",
+	height: 6,
+	calcBmi: function(){
+		this.bmi = this.mass /(this.height * this.height);
+		return this.bmi;
+	} 
+};
+
+
+if (johnInfo.calcBmi() > markInfo.calcBmi()) {
+	console.log("John\'s BMI is larger than Mark\'s at "+ johnInfo.bmi);
+} else if (johnInfo.calcBmi() < markInfo.calcBmi()) {
+	console.log("Mark\'s BMI is larger than John\'s at "+ markInfo.bmi);
+} else {
+	console.log("Mark and John has the same BMI of "+ markInfo.bmi);
+}
+*/
